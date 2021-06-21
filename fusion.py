8f1f3da42332bb4ed48f3bd386530e9c5d0d54cb
Juhi Purswani <juhi.purswani7@gmail.com>
2021-06-22 04:06:08 +0530

    nb_removed!

--- fusion.py
+++ fusion.py
@@ -18,16 +18,14 @@ from keras.optimizers import RMSprop
 import tensorflow as tf
 
 def multi_modal_network(input_shape):
-    nb_filter = [6, 12]
-    kernel_size = 3
-    seq = Sequential()
-    seq.add(Convolution2D(nb_filter[0], kernel_size, kernel_size, input_shape=input_shape,
-                          border_mode='valid', dim_ordering='th'))
-    seq.add(Activation('relu'))
-    seq.add(MaxPooling2D(pool_size=(2, 2)))  
-    seq.add(Dropout(.25))
-    seq.add(Flatten())
-    seq.add(Dense(128, activation='relu'))
-    seq.add(Dropout(0.1))
-    seq.add(Dense(50, activation='relu'))
-    return seq
+   kernel_size = 3
+   seq = Sequential()
+   seq.add(Convolution2D(6, kernel_size=(3,3), input_shape=(27, 37, 32)))
+   seq.add(Activation('relu'))
+   seq.add(MaxPooling2D(pool_size=(2, 2)))
+   seq.add(Dropout(.25))
+   seq.add(Flatten())
+   seq.add(Dense(128, activation='relu'))
+   seq.add(Dropout(0.1))
+   seq.add(Dense(50, activation='relu'))
+   return seq
